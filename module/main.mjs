6a4ac730b73bddf7c2e84770cc9a6dd088963933
chadvandy <chadvandewettering@gmail.com>
2024-12-02 22:04:42 -0500

    Initial commit

---
+++ module/main.mjs
@@ -0,0 +1,21 @@
+console.log("Hello Foundry!")
+
+Hooks.on("init", function() {
+    console.log("init event triggered.")
+})
+
+Hooks.on("ready", function() {
+    console.log("ready event trggered.")
+})
+
+// Entry point for the Legacy system.
+
+// TODO Init on GitHub
+// TODO Draft up needed functionality
+
+// TODO Create sheets for all relevant components
+// TODO Reference existing examples:
+/*
+https://github.com/philote/masks-newgeneration-unofficial/blob/main/templates/sheets/actor-sheet.hbs
+https://github.com/asacolips-projects/pbta/blob/main/src/templates/actors/actor-sheet.html
+*/
